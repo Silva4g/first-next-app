66d83acc717283c234fd480b3a011b2e29985eaa
silva4g <kauanssousa@gmail.com>
2021-04-20 13:42:45 -0300

    :tada: first commit

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable import/prefer-default-export */
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361"/>
+      Kauan Silva
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417"/>
+      Sign in with GitHub
+    </button>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import styles from './home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Home | ig.news</title>
+    </Head>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span>React</span> world. </h1>
+        <p>
+          Get access to all the publications <br/>
+          <span>for $9.90 month</span>
+        </p>
+        <SubscribeButton />
+      </section>
+      <img src="/images/avatar.svg" alt="Girl coding"/>
+    </main>
+    </>
+  );
+}
